a4c9fbf433444d3becc98a53df9555bbe8fd800d
Vitaly Orekhov <crce2000@mail.ru>
2024-08-20 22:40:59 +0300

    [WLANWIZ] Fix drawing code on GCC

--- dll/shellext/wlanwiz/advsettings.cpp
+++ dll/shellext/wlanwiz/advsettings.cpp
@@ -11,17 +11,39 @@ LRESULT CWlanWizard::OnAdvancedSettings(WORD wNotifyCode, WORD wID, HWND hWndCtl
 {
 	ATL::CStringW cswNetworkAdapterPath = L"";
 	LPOLESTR lpwszNetConCLSID;
+	CComPtr<IShellFolder> sfConn;
+	LPITEMIDLIST pidl = NULL;
+	LPCITEMIDLIST pidlChild = NULL;
+	SHDESCRIPTIONID did = { 0 };
 
-	HRESULT hr = StringFromIID(CLSID_NetworkConnections, &lpwszNetConCLSID);
+	HRESULT hr = SHGetSpecialFolderLocation(NULL, CSIDL_CONNECTIONS, &pidl);
 
-	if (SUCCEEDED(hr))
-	{
-		cswNetworkAdapterPath.Format(L"::%s\\::%s", lpwszNetConCLSID, this->m_sGUID);
+	if (FAILED(hr))
+		goto Exit;
 
-		/* NT 5.1 & 5.2 would still try to open the property sheet if you unplug the adapter. */
-		ShellExecuteW(NULL, L"properties", cswNetworkAdapterPath, NULL, NULL, SW_SHOWNORMAL);
-	}
+	hr = SHBindToParent(pidl, IID_IShellFolder, reinterpret_cast<LPVOID*>(&sfConn), &pidlChild);
 
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = SHGetDataFromIDListW(sfConn, pidlChild, SHGDFIL_DESCRIPTIONID, &did, sizeof(did));
+
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = StringFromIID(did.clsid, &lpwszNetConCLSID);
+
+	if (FAILED(hr))
+		goto Exit;
+
+	ILFree(pidl);
+	sfConn.Release();
+	cswNetworkAdapterPath.Format(L"::%s\\::%s", lpwszNetConCLSID, this->m_sGUID);
+
+	/* NT 5.1 & 5.2 would still try to open the property sheet if you unplug the adapter. */
+	ShellExecuteW(NULL, L"properties", cswNetworkAdapterPath, NULL, NULL, SW_SHOWNORMAL);
+
+Exit:
 	this->ShowWindow(SW_HIDE);
 	this->SendMessageW(WM_CLOSE);
 
