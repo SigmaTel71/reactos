9bcbcb4598139105c106eaed1dc9aab397cd0f80
Vitaly Orekhov <crce2000@mail.ru>
2024-08-20 22:40:59 +0300

    [WLANWIZ] Fix drawing code on GCC and hardcode CLSID_NetworkConnections
    When `SHGDFIL_DESCRIPTIONID` will be supported, this hardcode will be
    removed in favor of CSIDL_CONNECTIONS approach. Though no one will ever
    change the CLSID for that applet... unless?

--- dll/shellext/wlanwiz/advsettings.cpp
+++ dll/shellext/wlanwiz/advsettings.cpp
@@ -11,17 +11,49 @@ LRESULT CWlanWizard::OnAdvancedSettings(WORD wNotifyCode, WORD wID, HWND hWndCtl
 {
 	ATL::CStringW cswNetworkAdapterPath = L"";
 	LPOLESTR lpwszNetConCLSID;
+#if !defined(__REACTOS__)
+	CComPtr<IShellFolder> sfConn;
+	LPITEMIDLIST pidl = NULL;
+	LPCITEMIDLIST pidlChild = NULL;
+	SHDESCRIPTIONID did = { 0 };
 
+	HRESULT hr = SHGetSpecialFolderLocation(NULL, CSIDL_CONNECTIONS, &pidl);
+
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = SHBindToParent(pidl, IID_IShellFolder, reinterpret_cast<LPVOID*>(&sfConn), &pidlChild);
+
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = SHGetDataFromIDListW(sfConn, pidlChild, SHGDFIL_DESCRIPTIONID, &did, sizeof(did));
+
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = StringFromIID(did.clsid, &lpwszNetConCLSID);
+
+	if (FAILED(hr))
+		goto Exit;
+
+	ILFree(pidl);
+	sfConn.Release();
+#else
+#if _WIN32_WINNT < _WIN32_WINNT_VISTA
 	HRESULT hr = StringFromIID(CLSID_NetworkConnections, &lpwszNetConCLSID);
+#else
+	HRESULT hr = StringFromIID(CLSID_ConnectionFolder, &lpwszNetConCLSID);
+#endif
+	if (FAILED(hr))
+		goto Exit;
+#endif
+	cswNetworkAdapterPath.Format(L"::%s\\::%s", lpwszNetConCLSID, this->m_sGUID);
 
-	if (SUCCEEDED(hr))
-	{
-		cswNetworkAdapterPath.Format(L"::%s\\::%s", lpwszNetConCLSID, this->m_sGUID);
+	/* NT 5.1 & 5.2 would still try to open the property sheet if you unplug the adapter. */
+	ShellExecuteW(NULL, L"properties", cswNetworkAdapterPath, NULL, NULL, SW_SHOWNORMAL);
 
-		/* NT 5.1 & 5.2 would still try to open the property sheet if you unplug the adapter. */
-		ShellExecuteW(NULL, L"properties", cswNetworkAdapterPath, NULL, NULL, SW_SHOWNORMAL);
-	}
-
+Exit:
 	this->ShowWindow(SW_HIDE);
 	this->SendMessageW(WM_CLOSE);
 
