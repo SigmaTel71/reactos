df8fccd15bd6b94ef262ddff8c4816b26a2702eb
Vitaly Orekhov <vkvo2000@vivaldi.net>
2025-01-11 22:17:04 +0300

    [WLANWIZ] Compact `FAILED()` usages

--- dll/shellext/wlanwiz/advsettings.cpp
+++ dll/shellext/wlanwiz/advsettings.cpp
@@ -2,9 +2,8 @@
  * PROJECT:     ReactOS Shell
  * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
  * PURPOSE:     ReactOS Wizard for Wireless Network Connections (WLAN Advanced Settings)
- * COPYRIGHT:   Copyright 2024 Vitaly Orekhov <vkvo2000@vivaldi.net>
+ * COPYRIGHT:   Copyright 2024-2025 Vitaly Orekhov <vkvo2000@vivaldi.net>
  */
-
 #include "main.h"
 
 LRESULT CWlanWizard::OnAdvancedSettings(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
@@ -16,24 +15,16 @@ LRESULT CWlanWizard::OnAdvancedSettings(WORD wNotifyCode, WORD wID, HWND hWndCtl
 	LPCITEMIDLIST pidlChild = NULL;
 	SHDESCRIPTIONID did = { 0 };
 
-	HRESULT hr = SHGetSpecialFolderLocation(NULL, CSIDL_CONNECTIONS, &pidl);
-
-	if (FAILED(hr))
+	if (FAILED(SHGetSpecialFolderLocation(NULL, CSIDL_CONNECTIONS, &pidl)))
 		goto Exit;
 
-	hr = SHBindToParent(pidl, IID_IShellFolder, reinterpret_cast<LPVOID*>(&sfConn), &pidlChild);
-
-	if (FAILED(hr))
+	if (FAILED(SHBindToParent(pidl, IID_IShellFolder, reinterpret_cast<LPVOID*>(&sfConn), &pidlChild)))
 		goto Exit;
 
-	hr = SHGetDataFromIDListW(sfConn, pidlChild, SHGDFIL_DESCRIPTIONID, &did, sizeof(did));
-
-	if (FAILED(hr))
+	if (FAILED(SHGetDataFromIDListW(sfConn, pidlChild, SHGDFIL_DESCRIPTIONID, &did, sizeof(did))))
 		goto Exit;
 
-	hr = StringFromIID(did.clsid, &lpwszNetConCLSID);
-
-	if (FAILED(hr))
+	if (FAILED(StringFromIID(did.clsid, &lpwszNetConCLSID)))
 		goto Exit;
 
 	ILFree(pidl);
